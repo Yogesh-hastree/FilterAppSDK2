7008d9e7f21542f8efa633acb8006e60c40e0b7d
yogesh-hastree <yogeshkumar.hastree@gmail.com>
2023-05-10 17:40:53 +0530

    public

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/AFathi/ARVideoKit.git", exact: "1.5.51")
+        Package.Dependency
         .package(name: "Firebase",
                    url: "https://github.com/firebase/firebase-ios-sdk.git",
                    from: "8.0"),
